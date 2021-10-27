1bc11e5456388b7d872206ae1d57489a1eef8f7e
jorge <jorge.garcia.mdz@gmail.com>
2021-10-27 08:13:27 -0300

    agregando dependencia cors

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 const port = parseInt(process.env.PORT, 10) || 3000;
 
 app.use(logger('dev'));
+app.use(cors());
+app.options('*',cors());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
